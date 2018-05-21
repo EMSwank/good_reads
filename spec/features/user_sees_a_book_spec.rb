11c2cd4d150a99df5e5767a646f70808db52a20d
Eliot Swank <eliotswank@gmail.com>
2018-05-21 14:16:08 -0600

    fixes test, adds controller

--- spec/features/user_sees_a_book_spec.rb
+++ spec/features/user_sees_a_book_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe "User Visits a book show page " do
   it " and sees a book title and list of reviews" do
 
-  book = Book.new(title: "Foo")
+  book = Book.create(title: "Foo")
 
-  visit book_path
+  visit book_path(book)
 
   expect(page).to have_content("Foo")
   end
